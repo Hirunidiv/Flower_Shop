d1926d7d7582d7641c50cf14e052675292148f88
Chanodyapraveen <135603936+Chanodyapraveen@users.noreply.github.com>
2025-08-12 13:55:10 +0530

    Add responsive slider and category sections with styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
+import HomePage from './pages/HomePage'
 import Shop from './pages/Shop'
 import Contact from './pages/contact'
 import Cart from './pages/Cart'
@@ -11,7 +11,7 @@ function App() {
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/shop" element={<Shop />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/cart" element={<Cart />} />
